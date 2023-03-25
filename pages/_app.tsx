31eb2915c10c9d268fa16efc3c799fdca1bdd7a4
nimaaskari <nimaa.dev@gmail.com>
2023-03-25 12:08:30 +0330

    add the header and 'ad' banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { store } from "../store";
@@ -5,8 +6,16 @@ import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        <title>Shopty</title>
+        <meta name="desctiption" content="shopty online shop" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 }

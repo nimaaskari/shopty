464969458942c680c8a4f3ded0694e35f09a76ea
nimaaskari <nimaa.dev@gmail.com>
2023-03-25 13:26:40 +0330

    add the top menu

---
+++ components/header/Top.tsx
@@ -0,0 +1,86 @@
+// import styles from "./styles.module.scss";
+// import { MdSecurity } from "react-icons/md";
+// import { BsSuitHeart } from "react-icons/bs";
+// import { RiAccountPinCircleLine } from "react-icons/ri";
+// import { RiArrowDropDownFill } from "react-icons/ri";
+
+// export default function Top() {
+//   return (
+//     <div className="styles.top">
+//       <div className={styles.top__container}>
+//         <div></div>
+//         <ul className={styles.top__list}>
+//           <li>
+//             <img
+//               src="https://static.vecteezy.com/system/resources/previews/011/571/454/non_2x/circle-flag-of-iran-free-png.png"
+//               alt="country flag"
+//             />
+
+//             <span>IRAN / USD</span>
+//           </li>
+//           <li>
+//             <MdSecurity />
+//             <span>Buyer Protection</span>
+//           </li>
+//           <li>
+//             <span>Customer Service</span>
+//           </li>
+//           <li>
+//             <span>Help</span>
+//           </li>
+//           <li>
+//             <BsSuitHeart />
+//             <span>Wishlist</span>
+//           </li>
+//           <li>
+//             <div className={styles.flex}>
+//               <RiAccountPinCircleLine />
+//               <span>Account</span>
+//               <RiArrowDropDownFill />
+//             </div>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
+
+import styles from "./styles.module.scss";
+import { MdSecurity } from "react-icons/md";
+import { BsSuitHeart } from "react-icons/bs";
+import Link from "next/link";
+export default function Top({}) {
+  return (
+    <div className={styles.top}>
+      <div className={styles.top__container}>
+        <div></div>
+        <ul className={styles.top__list}>
+          <li className={styles.li}>
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/011/571/454/non_2x/circle-flag-of-iran-free-png.png"
+              alt="country flag"
+            />
+            <span> / USD</span>
+          </li>
+          <li className={styles.li}>
+            <MdSecurity />
+            <span>Buyer Protection</span>
+          </li>
+          <li className={styles.li}>
+            <span>Customer Service</span>
+          </li>
+          <li className={styles.li}>
+            <span>Help</span>
+          </li>
+          <li className={styles.li}>
+            <BsSuitHeart />
+            <Link href="/profile/whishlist">
+              <span>Whishlist</span>
+            </Link>
+          </li>
+          <li className={styles.li}></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- next.config.cjs
+++ next.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-    prependData: `@import "./base.scss";`,
+    prependData: `@import "../../styles/base.scss";`,
   },
 };
 
